81a85e47d7933c5546b2bfe88a28c3ae46aae846
VK30 <59475436+VK30@users.noreply.github.com>
2020-02-09 23:19:53 +0500

    s

---
+++ imgresize.js
@@ -0,0 +1,9 @@
+var image=document.getElementById('primg')
+var function()=resizeFunc(){
+	image.clientWidth='700px';
+	image.clientHeight='400px';
+	image.style.position='absolute';
+	image.style.top='30%';
+	image.style.left='35%';
+
+}
